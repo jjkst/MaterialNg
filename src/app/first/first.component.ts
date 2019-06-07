a675dc57e6bf0ff974b6442d14b1651100e72a6f
Karthik Jayaraman <Karthi.Jayaraman@gmail.com>
2019-06-07 16:22:05 -0400

    card bro

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponent implements OnInit {
 
+  tiles: TeamScore[] = [
+    {text: 'ENG', cols: 2, rows: 1, color: 'skyblue'},
+    {text: '311/8', cols: 1, rows: 1, color: 'skyblue'},
+    {text: 'SA', cols: 2, rows: 1, color: 'lightgreen'},
+    {text: '207', cols: 1, rows: 1, color: 'lightgreen'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export interface TeamScore {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
 }
--- src/app/material.ts
+++ src/app/material.ts
@@ -1,8 +1,11 @@
-import {MatButtonModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule, 
+    MatToolbarModule, MatCardModule, MatGridListModule } from '@angular/material';
 import {NgModule} from '@angular/core'
 
 @NgModule({
-imports: [MatButtonModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule, MatToolbarModule],
-exports: [MatButtonModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule, MatToolbarModule]
+imports: [MatButtonModule, MatNativeDateModule, MatIconModule, MatSidenavModule, 
+    MatListModule, MatToolbarModule, MatCardModule,MatGridListModule],
+exports: [MatButtonModule, MatNativeDateModule, MatIconModule, MatSidenavModule, 
+    MatListModule, MatToolbarModule, MatCardModule,MatGridListModule]
 })
 export class MaterialModule{}
